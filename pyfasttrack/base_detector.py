[fix] fix ellipse precision

pyfasttrack/base_detector.py
@@ -1,8 +1,6 @@
 import abc
 import numpy as np
 import cv2
-import scipy
-import math
 
 
 class BaseDetector(metaclass=abc.ABCMeta):
@@ -56,28 +54,26 @@ class BaseDetector(metaclass=abc.ABCMeta):
             body = self.get_features(mask)
             is_left, rotated_mask, rot = self.get_direction(mask, body)
 
-            body_center = np.uintp(np.matmul(rot, np.asarray(
-                [body["center"][0], body["center"][1], 1]).reshape(3, 1)))
+            body_center = np.intp(
+                rot @ np.asarray([body["center"][0], body["center"][1], 1]))
             rot = cv2.invertAffineTransform(rot)
 
-            # TODO: precision not checking with FastTrack C++
-            mask_tail = rotated_mask[:, int(body_center[0, 0])::]
+            mask_tail = rotated_mask[:, body_center[0]::]
             tail = self.get_features(mask_tail)
-            a = np.matmul(rot, np.asarray(
-                [tail["center"][0] + body_center[0, 0], tail["center"][1] + body_center[1, 0], 1]).reshape(3, 1))
-            tail["center"][0] = a[0, 0] + coordinate[0]
-            tail["center"][1] = a[1, 0] + coordinate[1]
+            a = rot @ np.asarray(
+                [tail["center"][0] + body_center[0], tail["center"][1], 1])
+            tail["center"][0] = a[0] + coordinate[0]
+            tail["center"][1] = a[1] + coordinate[1]
             tail["orientation"] = tail["orientation"] - \
                 np.pi * (tail["orientation"] > np.pi)
             tail["orientation"] = self.modulo(
                 tail["orientation"] + body["orientation"] + np.pi * (np.abs(tail["orientation"]) > 0.5 * np.pi))
 
-            mask_head = rotated_mask[:, 0:int(body_center[0, 0])]
+            mask_head = rotated_mask[:, 0:body_center[0]]
             head = self.get_features(mask_head)
-            a = np.matmul(rot, np.asarray(
-                [head["center"][0], head["center"][1], 1]).reshape(3, 1))
-            head["center"][0] = a[0, 0] + coordinate[0]
-            head["center"][1] = a[1, 0] + coordinate[1]
+            a = rot @ np.asarray([head["center"][0], head["center"][1], 1])
+            head["center"][0] = a[0] + coordinate[0]
+            head["center"][1] = a[1] + coordinate[1]
             head["orientation"] = head["orientation"] - \
                 np.pi * (head["orientation"] > np.pi)
             head["orientation"] = self.modulo(
@@ -144,7 +140,7 @@ class BaseDetector(metaclass=abc.ABCMeta):
             Angle between 0->2pi
 
         """
-        return angle - 2 * np.pi * math.floor(angle / (2 * np.pi))
+        return angle - 2 * np.pi * np.floor(angle / (2 * np.pi))
 
     def get_direction(self, mask, features):
         """Get the object direction.