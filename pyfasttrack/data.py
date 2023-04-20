cb9d696df7bbfc5ebb03c8377a8dc76fce2e797f
bgallois <benjamin@gallois.cc>
2023-04-20 13:31:14 +0200

    [feat] add result writing

--- pyfasttrack/data.py
+++ pyfasttrack/data.py
@@ -2,6 +2,7 @@ import toml
 import sqlite3
 import pathlib
 import os
+import numpy as np
 
 
 class Configuration():
@@ -45,7 +46,8 @@ class Configuration():
 
         """
         try:
-            cnx = sqlite3.connect(pathlib.Path(os.path.abspath(path)).as_uri() + "?mode=ro", uri = True)
+            cnx = sqlite3.connect(pathlib.Path(
+                os.path.abspath(path)).as_uri() + "?mode=ro", uri=True)
             query = cnx.execute("SELECT parameter, value FROM parameter;")
             self.params = dict()
             for param, value in query:
@@ -55,7 +57,6 @@ class Configuration():
         except Exception as e:
             print(e)
             return None
-
 
     def write_toml(self, path):
         """Write a configuration file.
@@ -103,3 +104,47 @@ class Configuration():
 
         """
         return [self.params["parameters"][key] for key in keys]
+
+
+class Result():
+    """Class to write result files compatible with FastTrack.
+
+    """
+
+    def add_data(self, data):
+        """Append data in the database.
+
+            Parameters
+            ----------
+            data : dict
+                Data.
+
+        """
+        query = self.cnx.execute("INSERT INTO tracking(xHead, yHead, tHead, xTail, yTail, tTail, xBody, yBody, tBody, curvature, areaBody,"
+                                 "perimeterBody, headMajorAxisLength, headMinorAxisLength, headExcentricity, tailMajorAxisLength,"
+                                 "tailMinorAxisLength, tailExcentricity, bodyMajorAxisLength, bodyMinorAxisLength, bodyExcentricity,"
+                                 "imageNumber, id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ",
+                                 (data["head"]["center"][0], data["head"]["center"][1], data["head"]["orientation"],
+                                  data["tail"]["center"][0], data["tail"]["center"][1], data["tail"]["orientation"],
+                                  data["body"]["center"][0], data["body"]["center"][1], data["body"]["orientation"],
+                                  data["data"]["curv"], data["data"]["area"], data["data"]["perim"],
+                                  data["head"]["major_axis"], data["head"]["minor_axis"], np.sqrt(
+                                     1-(data["head"]["minor_axis"]/data["head"]["major_axis"])**2),
+                                  data["tail"]["major_axis"], data["tail"]["minor_axis"], np.sqrt(
+                                     1-(data["tail"]["minor_axis"]/data["tail"]["major_axis"])**2),
+                                  data["body"]["major_axis"], data["body"]["minor_axis"], np.sqrt(
+                                     1-(data["body"]["minor_axis"]/data["body"]["major_axis"])**2),
+                                  data["info"]["time"], data["info"]["id"]))
+
+    def __init__(self, path):
+        self.cnx = sqlite3.connect(os.path.abspath(path) + "/tracking.db")
+        query = self.cnx.execute("CREATE TABLE tracking ( xHead REAL, yHead REAL, tHead REAL, xTail REAL,"
+                                 "yTail REAL, tTail REAL, xBody REAL, yBody REAL, tBody REAL,"
+                                 "curvature REAL, areaBody REAL, perimeterBody REAL,"
+                                 "headMajorAxisLength REAL, headMinorAxisLength REAL,"
+                                 "headExcentricity REAL, tailMajorAxisLength REAL, tailMinorAxisLength REAL,"
+                                 "tailExcentricity REAL, bodyMajorAxisLength REAL, bodyMinorAxisLength REAL,"
+                                 "bodyExcentricity REAL, imageNumber INTEGER, id INTEGER)")
+
+    def __del__(self):
+        self.cnx.close()
--- pyfasttrack/test/test_data.py
+++ pyfasttrack/test/test_data.py
@@ -2,6 +2,7 @@ import toml
 import data
 import deepdiff
 import os
+import shutil
 
 
 def test_write_configuration():
@@ -35,3 +36,14 @@ def test_read_wrong():
     test = conf.read_toml("./test/data/cfg_.toml")
     assert test == None
 
+def test_add_data_result():
+    os.mkdir("./test/data/tmp/")
+    result = data.Result("./test/data/tmp/")
+    dat = dict()
+    dat["head"] = {"center": (0, 0), "orientation": 0, "minor_axis": 1, "major_axis": 1}
+    dat["body"] = {"center": (0, 0), "orientation": 0, "minor_axis": 1, "major_axis": 1}
+    dat["tail"] = {"center": (0, 0), "orientation": 0, "minor_axis": 1, "major_axis": 1}
+    dat["data"] = {"curv": 0, "area": 1, "perim": 1}
+    dat["info"] = {"time": 1, "id": 1}
+    test = result.add_data(dat)
+    shutil.rmtree("./test/data/tmp/")
