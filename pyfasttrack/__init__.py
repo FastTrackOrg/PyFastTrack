18d15481fbba7820573bce943da437869c2e22bc
bgallois <benjamin@gallois.cc>
2023-05-22 17:27:09 +0200

    [feat] add sentry for dev phase

--- pyfasttrack/__init__.py
+++ pyfasttrack/__init__.py
@@ -1,1 +1,9 @@
+import sentry_sdk
+sentry_sdk.init(
+    dsn="https://5dff536b23ee400db3e2f247edf1d711@o4505228290031616.ingest.sentry.io/4505228343967744",
 
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyfasttrack",
-    version="0.0.2",
+    version="0.0.3",
     author="Benjamin Gallois",
     author_email="benjamin.gallois@fasttrack.sh",
     description="Easy-to-use solution to integrate the tracking technology of the FastTrack software in Python projects.",
@@ -13,6 +13,7 @@ setup(
         'opencv-python',
         'scipy',
         'toml',
+        'sentry-sdk',
         'numpy',],
     license='MIT',
     python_requires='>=3.9',
